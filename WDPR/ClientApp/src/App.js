2d9f05924b0344aff5e649335766e13c06096eb4
janpernot <21116725@student.hhs.nl>
2023-01-04 16:51:03 +0100

    Frontend van de home page en routing naar de andere componenten

--- WDPR/ClientApp/src/App.js
+++ WDPR/ClientApp/src/App.js
@@ -4,6 +4,8 @@ import Voorstelling from "./components/Voorstelling";
 import './custom.css';
 import Tickets from "./components/Tickets"
 import Homepage from './components/Homepage';
+import Doneer from './components/Doneer';
+import Inloggen from './components/Inloggen';
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path='/' element={<Homepage />}></Route>
         <Route path='/Voorstelling' element={<Voorstelling />}></Route>
         <Route path='/Tickets' element={<Tickets />}></Route>
+        <Route path='/doneren' element={<Doneer />}></Route>
+        <Route path='/inloggen' element={<Inloggen />}></Route>
       </Routes>
     </>
   );
--- WDPR/ClientApp/src/components/Homepage.js
+++ WDPR/ClientApp/src/components/Homepage.js
@@ -3,8 +3,7 @@ import Logo from "../../src/HomeImages/Logo_theater_laak_V2.png";
 import Donate_icoon from "../../src/HomeImages/Donate_icoon.png";
 import Login_icoon from "../../src/HomeImages/Login_icoon.png";
 import Tickets_icoon from "../../src/HomeImages/Ticket_Icoon.png";
-
-// import Link from "react-router-dom";
+import ContentKaart from "./ContentKaart";
 import "./HomePage.css";
 
 export default function Homepage() {
@@ -20,23 +19,15 @@ export default function Homepage() {
                     </div>
                 </section>
                 <section className="Section2">
+                    <ContentKaart header="Doneren" icoon={Donate_icoon} actie="Doneer" paragraph="Steun Theater Laak om onze voorstellingen betaalbaar te houden voor iedereen!" link="/doneren" />
+                    <ContentKaart header="Tickets" icoon={Tickets_icoon} actie="Tickets" paragraph="Koop hier je tickets voor je favoriete voorstelling bij Theater Laak" link="/tickets" />
+                    <ContentKaart header="Inloggen" icoon={Login_icoon} actie="Inloggen" paragraph="Login bij je account" link="/inloggen" />
+                </section>
+                <section className="Section3">
                     <div>
-                        <h2>Doneren</h2>
-                        <img class="Icoon" src={Donate_icoon} alt="Doneer"></img>
-                        <p>Steun Theater Laak om onze voorstellingen betaalbaar te houden voor iedereen!</p>
-                        <button></button>
-                    </div>
-                    <div>
-                        <h2>Tickets</h2>
-                        <img class="Icoon" src={Tickets_icoon} alt="Tickets"></img>
-                        <p>Koop hier je tickets voor je favoriete voorstelling bij Theater Laak</p>
-                        <button></button>
-                    </div>
-                    <div>
-                        <h2>Doneren</h2>
-                        <img class="Icoon" src={Login_icoon} alt="Login"></img>
-                        <p>Login bij je account</p>
-                        <button></button>
+                        <h2>Over Theater Laak</h2>
+                        <p>Theater Laak is een theater in het Haagse Stadsdeel Laak. Met dank aan onze donateurs weten wij al jaren betaalbare theatervoorstellingen te verzorgen voor al onze gasten. Van muziekoptredens tot theatervoorstellingen, alles wordt verzorgd bij Theater Laak. Lijkt het je leuk om een van onze voorstellingen te bezoeken? Neem dan een kijkje bij onze voorstellingen en koop je ticket zolang die beschikbaar is want op is op!</p>
+                        <button className="button V2">OVER ONS</button>
                     </div>
                 </section>
             </body>
