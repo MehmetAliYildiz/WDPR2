da8b6ea67a5d8041f0201d8ec54dead387cca062
BuildTools <unconfigured@null.spigotmc.org>
2023-01-10 09:50:38 +0100

    Toevoeging reserveerknop op home pagina

--- WDPR/ClientApp/src/components/Homepage.js
+++ WDPR/ClientApp/src/components/Homepage.js
@@ -3,6 +3,8 @@ import Logo from "../../src/HomeImages/Logo_theater_laak_V2.png";
 import Donate_icoon from "../../src/HomeImages/Donate_icoon.png";
 import Login_icoon from "../../src/HomeImages/Login_icoon.png";
 import Tickets_icoon from "../../src/HomeImages/Ticket_Icoon.png";
+import Zaal1 from "../../src/HomeImages/Zaal_1.jpg";
+import { Link } from 'react-router-dom';
 import ContentKaart from "./ContentKaart";
 import "./HomePage.css";
 
@@ -28,6 +30,24 @@ export default function Homepage() {
                         <h2>Over Theater Laak</h2>
                         <p>Theater Laak is een theater in het Haagse Stadsdeel Laak. Met dank aan onze donateurs weten wij al jaren betaalbare theatervoorstellingen te verzorgen voor al onze gasten. Van muziekoptredens tot theatervoorstellingen, alles wordt verzorgd bij Theater Laak. Lijkt het je leuk om een van onze voorstellingen te bezoeken? Neem dan een kijkje bij onze voorstellingen en koop je ticket zolang die beschikbaar is want op is op!</p>
                         <button className="button V2">OVER ONS</button>
+                    </div>
+                </section>
+                <section className="Section4">
+                    <div className="ContentAside">
+                        <img src={Zaal1} />
+                    </div>
+                    <div className="ContentText">
+                        <div style={{ width: "300px" }}>
+                            <h2>
+                                Eigen zaal huren?
+                            </h2>
+                            <p>
+                                Bij theater laak hebben wij een aantal zalen beschikbaar voor gasten.
+                            </p>
+                            <Link to="/reserveren">
+                                <button className="buttonB">RESERVEER NU</button>
+                            </Link>
+                        </div>
                     </div>
                 </section>
             </body>
