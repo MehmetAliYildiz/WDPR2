7a5e84466bf711a3729fa14fd4682e70d4772241
Jan Pernot <21116725@student.hhs.nl>
2023-01-19 16:24:36 +0100

    frontend van voorstellingdetail pagina

--- WDPR/ClientApp/src/components/VoorstellingPagina/VoorstellingDetail.js
+++ WDPR/ClientApp/src/components/VoorstellingPagina/VoorstellingDetail.js
@@ -6,7 +6,7 @@ import Footer from "../navFoot/Footer";
 function VoorstellingDetail(prop) {
     
     const [voorstelling, setVoorstelling] = useState([]);
-    const [zaal, setZaal] = useState([]);
+    const [agendas, setAgendas] = useState([]);
 
     useEffect(() => {
         const queryParameters = new URLSearchParams(window.location.search);
@@ -16,17 +16,26 @@ function VoorstellingDetail(prop) {
             setVoorstelling(res.data);
             console.log(res.data);
         });
-    }, []);
 
-    useEffect(() => { 
-        
-        Axios.get(`https://localhost:7260/api/Zaal/${zaal.ZaalId}`).then((res) => {
-            setZaal(res.data);
+        Axios.get(`https://localhost:7260/api/agenda/voorstelling/${voorstellingId}`)
+        .then((res) => {
+            // setAgendas([
+            //     { id: 1, title: 'Meeting with team' },
+            //     { id: 2, title: 'Lunch with client' },
+            //     { id: 3, title: 'Prepare for presentation' },
+            // ]);
             console.log(res.data);
+            for(var i = 0; i < res.data.lenght; i++){
+                agendas.push(res.data[i]);
+            }
+
+            // setAgendas(res.data);
+            // console.log(res.data);
+            console.log(agendas)
         });
+
     }, []);
         
-    
     return (
     <>
         < Navigatie />
@@ -35,6 +44,14 @@ function VoorstellingDetail(prop) {
             <img src={voorstelling.img} alt="..." />
             <p>{voorstelling.beschrijving}</p>
             <p>{voorstelling.ZaalId}</p>
+
+            {agendas.map((agenda) => (
+    <div key={agenda.Id}>
+        <p>{agenda.StartDatumTijd}</p>
+        <p>{agenda.EindDatumTijd}</p>
+        <p>{agenda.ZaalId}</p>
+    </div>
+))}
         </div>
         < Footer />
     </>
