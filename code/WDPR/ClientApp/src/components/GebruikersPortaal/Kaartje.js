4b2268c171ed6edcf5a5a066c5b19234e9164ca8
alexblaauw <alexblaauw44@gmail.com>
2023-01-29 22:48:57 +0100

    error fix

--- code/WDPR/ClientApp/src/components/GebruikersPortaal/Kaartje.js
+++ code/WDPR/ClientApp/src/components/GebruikersPortaal/Kaartje.js
@@ -6,36 +6,41 @@ function Kaartje(props) {
     const [print, setPrint] = useState(false);
     const [kaartje, setKaartje] = useState(props.kaartje);
 
-  const handlePrint = () => {
-    setPrint(true);
-    var prinen = document.getElementById("ticket")
-    window.print();
-    setPrint(false);
-  }
+    const handlePrint = () => {
+        setPrint(true);
+        var prinen = document.getElementById("ticket")
+        window.print();
+        setPrint(false);
+    }
 
-  const printen ={
-      width: '100%',
-      fontSize : '20px',
-      padding: '20px'
-}
+    const printen ={
+        width: '100%',
+        fontSize : '20px',
+        padding: '20px'
+    }
 
-  return (
-    <div>
-        <div className="card " key={kaartje.Id}>
-            <div id='ticket' className={" text-center ticket ${printen ? 'print-ticket' : ''}"} >
-                <QRCode value={kaartje.code} />
-                <div className="card-body">
-                    <div>naam: {kaartje.bestelling.gebruiker.userName}</div>
-                    <div>start tijd: {new Date(kaartje.agenda.startDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
-                    <div>eind tijd: {new Date(kaartje.agenda.eindDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
-                    <div>kaartje code: {kaartje.code}</div>
+    const getNaam = () => {
+        return kaartje.bestelling.gebruiker == null ? ("") :
+            (<div>naam: {kaartje.bestelling.gebruiker.userName}</div>);
+    }
+
+    return (
+        <div>
+            <div className="card " key={kaartje.Id}>
+                <div id='ticket' className={" text-center ticket ${printen ? 'print-ticket' : ''}"} >
+                    <QRCode value={kaartje.code} />
+                    <div className="card-body">
+                        {getNaam()}
+                        <div>start tijd: {new Date(kaartje.agenda.startDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
+                        <div>eind tijd: {new Date(kaartje.agenda.eindDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
+                        <div>kaartje code: {kaartje.code}</div>
+                    </div>
                 </div>
+                <button className="btn btn-primary" onClick={handlePrint}>Print</button>
+                <button className="btn btn-primary" onClick={handlePrint}>Agenda</button>
             </div>
-            <button className="btn btn-primary" onClick={handlePrint}>Print</button>
-            <button className="btn btn-primary" onClick={handlePrint}>Agenda</button>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Kaartje;
