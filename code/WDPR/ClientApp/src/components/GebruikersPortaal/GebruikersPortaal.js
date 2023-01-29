2abc9ea4f3fc9adafe00f89e52c918f6ea6d86be
alexblaauw <alexblaauw44@gmail.com>
2023-01-29 22:21:46 +0100

    Kaartje mapbaar gemaakt

--- code/WDPR/ClientApp/src/components/GebruikersPortaal/GebruikersPortaal.js
+++ code/WDPR/ClientApp/src/components/GebruikersPortaal/GebruikersPortaal.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from "react";
-import QrCode from './qrCode';
+import QrCode from './Kaartje';
 import Footer from "../navFoot/Footer";
 import NavBar from "../navFoot/navbar"
+import Kaartje from "./Kaartje";
+import { Navigate } from "react-router-dom";
 
 function GebruikersPortaal() {
     const [kaartjes, setKaartjes] = useState([]);
     const [error, setError] = useState('');
+    const [redirect, setRedirect] = useState("");
 
     useEffect(() => {
         // Get the email from the user's session
         const email = sessionStorage.getItem("gebruikersNaam");
 
+        if (email == null) setRedirect("/inloggen");
         // Fetch the kaartjes when the component mounts
         fetch(`https://localhost:7260/Kaartje/kaartjeBijGebruiker/${email}`,{
             method: 'GET',
@@ -30,15 +34,25 @@ function GebruikersPortaal() {
             .catch(err => setError(err.message));
     }, []);
 
-    return (
+    const getKaartjes = () => {
+        return(kaartjes.map(kaartje => {
+            return (
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <Kaartje kaartje={kaartje} />
+                </div>
+            );
+        }));
+    }
+
+    return redirect == "" ? (
         <>
             <NavBar/>
             <div className="container">
                 <h1 className="text-center my-5">Kaartjes</h1>
                 {error && <p className="text-danger">{error}</p>}
                 {kaartjes.length > 0 ? (
-                    <div className="row row-cols-1 row-cols-md-3 g-4">
-                        <QrCode kaartjes={kaartjes}/>
+                    <div>
+                        { getKaartjes() }
                     </div>
                 ) : (
                     <p className="text-center">Er zijn nog geen kaartjes gekoppeld aan uw account</p>
@@ -47,6 +61,8 @@ function GebruikersPortaal() {
 
             <Footer/>
         </>
+    ) : (
+            <Navigate to={redirect}/>
     );
 }
 
--- code/WDPR/ClientApp/src/components/GebruikersPortaal/Kaartje.js
+++ code/WDPR/ClientApp/src/components/GebruikersPortaal/Kaartje.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import QRCode from 'react-qr-code';
 
-function QrCode({ kaartjes }) {
-  const [print, setPrint] = useState(false);
+function Kaartje(props) {
+    const [print, setPrint] = useState(false);
+    const [kaartje, setKaartje] = useState(props.kaartje);
 
   const handlePrint = () => {
     setPrint(true);
@@ -20,25 +21,21 @@ function QrCode({ kaartjes }) {
 
   return (
     <div>
-      
-      {kaartjes.map(kaartje => (
-
         <div className="card " key={kaartje.Id}>
             <div id='ticket' className={" text-center ticket ${printen ? 'print-ticket' : ''}"} >
-                <QRCode value={kaartje.code}/>
+                <QRCode value={kaartje.code} />
                 <div className="card-body">
                     <div>naam: {kaartje.bestelling.gebruiker.userName}</div>
-                    <div>start tijd: {new Date(kaartje.agenda.startDatumTijd).toLocaleDateString(undefined, {hour: '2-digit', minute:'2-digit'})}</div>
-                    <div>eind tijd: {new Date(kaartje.agenda.eindDatumTijd).toLocaleDateString(undefined, {hour: '2-digit', minute:'2-digit'})}</div>
+                    <div>start tijd: {new Date(kaartje.agenda.startDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
+                    <div>eind tijd: {new Date(kaartje.agenda.eindDatumTijd).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' })}</div>
                     <div>kaartje code: {kaartje.code}</div>
                 </div>
             </div>
             <button className="btn btn-primary" onClick={handlePrint}>Print</button>
             <button className="btn btn-primary" onClick={handlePrint}>Agenda</button>
         </div>
-      ))}
     </div>
   );
 }
 
-export default QrCode;
+export default Kaartje;
